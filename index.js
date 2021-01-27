f59291fa65e6ab3285c00ef6b78a215f6602b534
lhagvadarjaa <lhagva98@gmail.com>
2021-01-27 18:11:14 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+var songs = [
+  {
+    "id": 1,
+    "url": "news.mn/page1",
+    "content": "mongoliin unertei salkhi"
+  },
+  {
+    "id": 2,
+    "url": "news.mn/page2",
+    "content": "eejin chanasan tsai"
+  },
+  {
+    "id": 3,
+    "url": "news.mn/page3",
+    "content": "tsergiin bodol"
+  },
+  {
+    "id": 4,
+    "url": "news.mn/page4",
+    "content": "Bi chamd hairtai"
+  },
+  {
+    "id": 5,
+    "url": "news.mn/page5",
+    "content": "Altan narnii tsatsrag"
+  }
+]
+///Өгөгдсөн дууны нэрийг songs object дотроос хайж тухайн дууны id буцаана
+//Content string дотор тухайн songName орсон байх шаардлагатай бөгөөд том жижиг үсгийг адилхан гэж үзнэ ?
+//Хэрвээ олон байвал хамгийн эхэнд олдсоныг буцаана
+
+function search(songName) {
+
+  return "some id";
+}
+
+
+console.log(search(process.argv[2]))
